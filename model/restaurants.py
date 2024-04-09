766c34b2986b336e190bfa60bb8abc14c6b113fa
emilyrod3949 <156032703+emilyrod3949@users.noreply.github.com>
2024-04-09 13:56:20 -0400

    Add files via upload

---
+++ model/restaurants.py
@@ -0,0 +1,9 @@
+class Restaurant:
+    def__init__(name, rating, review, price, location):
+        self.name = name
+        self.rating = rating
+        self.review = review
+        self.price = price
+        self.location = location
+        self.adjust = []
+    
