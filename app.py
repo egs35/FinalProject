f4cac88a9c8255690f7301a5b8ca9c1de795c68d
Rully Agus Hendrawan <ruhendrawan@pitt.edu>
2024-04-12 15:12:43 -0400

    flask /restaurant controller

--- app.py
+++ app.py
@@ -1,17 +1,58 @@
 import sqlite3
-from flask import Flask
 
-@app.route('/')
+from flask import Flask, render_template, g
+from model.restaurants import Restaurant
+
+
+app = Flask(__name__)
+
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect("restaurants.db")
+    return g.db
+
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
+
+@app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route('/restaurants')
+
+@app.route("/restaurants")
 def restaurants():
-    return restaurants_view()
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM restaurants")
+    restaurants = cursor.fetchall()
+    res_obj = []
+    for restaurant in restaurants:
+        res_obj.append(
+            Restaurant(
+                name=restaurant[1],
+                rating=restaurant[2],
+                review=restaurant[3],
+                price=restaurant[4],
+                location=restaurant[5],
+            )
+        )
+    return render_template("restaurants.html", restaurants=res_obj)
 
-@app.route('/add')
-def add():
-    return add_view()
 
-if __name__ == '__main__':
+@app.route('/restaurants_reviews/<name>')
+def restaurant_review_view(name):
+    pass
+#     return restaurants_reviews_view()
+
+# @app.route('/add')
+# def add():
+#     return add_view()
+
+if __name__ == "__main__":
     app.run(debug=True)
--- model/restaurants.py
+++ model/restaurants.py
@@ -1,9 +1,9 @@
 class Restaurant:
-    def__init__(name, rating, review, price, location):
+    def __init__(self, name, rating, review, price, location):
         self.name = name
-        self.rating = rating
-        self.review = review
-        self.price = price
+        self.rating = rating # summary of the rating
+        self.review = review # summary of the review
+        self.price = price # summary of the price
         self.location = location
         self.adjust = []
     
