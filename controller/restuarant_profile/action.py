ecf8c6c4d292522e263e04adae9afe5b8bc2eb3f
emilyrod3949 <156032703+emilyrod3949@users.noreply.github.com>
2024-04-12 13:21:01 -0400

    Update action.py

--- controller/restuarant_profile/action.py
+++ controller/restuarant_profile/action.py
@@ -2,5 +2,5 @@ from flask import render_template
 from .action import create_restaurant_profile
 
 def profile_view():
-    pet = create_pet_profile("stack'd", 5 , "great burgers!" , $$ , Oakland)
+    restaurant = create_restaurant_profile("stack'd", 5 , "great burgers!" , $$ , Oakland)
     return render_template("profile.html", pet=pet)
