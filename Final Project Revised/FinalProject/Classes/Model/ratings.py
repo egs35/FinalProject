29b189419c4c75a295861ea32506d89074214d0c
egs35 <156032659+egs35@users.noreply.github.com>
2024-04-24 15:53:44 -0400

    Delete Final Project Revised directory

--- Final Project Revised/FinalProject/Classes/Model/ratings.py
+++
@@ -1,3 +0,0 @@
-class Ratings:
-    def __init__(self, rating1):
-        self.rating1 = rating1
--- Final Project Revised/FinalProject/model/restaurants_reviews.py
+++
@@ -1,7 +0,0 @@
-class RestaurantReview:
-    def __init__(self, name, restaurant,rating, review_text, reviewer):
-       self.restaurant = restaurant # restaurant object
-       self.name = name
-       self.rating = rating 
-       self.review_text = review_text        
-       self.reviewer = reviewer
